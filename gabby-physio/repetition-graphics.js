f0d32b8a49d283519c29b9084c438272bac5a4c7
Jenia Nemzer <jenian@jenian-mbp-1.local>
2026-07-29 09:11:06 -0500

    new graphics

--- gabby-physio/repetition-graphics.js
+++ gabby-physio/repetition-graphics.js
@@ -1,9 +1,11 @@
 window.REPETITION_GRAPHICS = [
+  "graphics/repititions/bambi1.png",
   "graphics/repititions/bunny1.png",
   "graphics/repititions/cat1.png",
   "graphics/repititions/cat2.png",
   "graphics/repititions/cat3.png",
   "graphics/repititions/cat4.png",
   "graphics/repititions/panda1.png",
-  "graphics/repititions/strawberry1.png"
+  "graphics/repititions/strawberry1.png",
+  "graphics/repititions/turtle1.png"
 ];
